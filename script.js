1e6220227337fedd438044a1355717eb63d5b840
VadymByi <karabincom@gmail.com>
2025-04-13 15:42:46 +0300

    Project update: improved README and UI

--- script.js
+++ script.js
@@ -7,111 +7,15 @@ let wrongAnswer = document.getElementById("wrongAnswer");
 
 let currentKey = "";
 
-// Объект с таблицей умножения от 1 до 10(отдельно равенство, отдельно ответ)
-const multiplicationTable = {
-  "1x1": 1,
-  "1x2": 2,
-  "1x3": 3,
-  "1x4": 4,
-  "1x5": 5,
-  "1x6": 6,
-  "1x7": 7,
-  "1x8": 8,
-  "1x9": 9,
-  "1x10": 10,
-  "2x1": 2,
-  "2x2": 4,
-  "2x3": 6,
-  "2x4": 8,
-  "2x5": 10,
-  "2x6": 12,
-  "2x7": 14,
-  "2x8": 16,
-  "2x9": 18,
-  "2x10": 20,
-  "3x1": 3,
-  "3x2": 6,
-  "3x3": 9,
-  "3x4": 12,
-  "3x5": 15,
-  "3x6": 18,
-  "3x7": 21,
-  "3x8": 24,
-  "3x9": 27,
-  "3x10": 30,
-  "4x1": 4,
-  "4x2": 8,
-  "4x3": 12,
-  "4x4": 16,
-  "4x5": 20,
-  "4x6": 24,
-  "4x7": 28,
-  "4x8": 32,
-  "4x9": 36,
-  "4x10": 40,
-  "5x1": 5,
-  "5x2": 10,
-  "5x3": 15,
-  "5x4": 20,
-  "5x5": 25,
-  "5x6": 30,
-  "5x7": 35,
-  "5x8": 40,
-  "5x9": 45,
-  "5x10": 50,
-  "6x1": 6,
-  "6x2": 12,
-  "6x3": 18,
-  "6x4": 24,
-  "6x5": 30,
-  "6x6": 36,
-  "6x7": 42,
-  "6x8": 48,
-  "6x9": 54,
-  "6x10": 60,
-  "7x1": 7,
-  "7x2": 14,
-  "7x3": 21,
-  "7x4": 28,
-  "7x5": 35,
-  "7x6": 42,
-  "7x7": 49,
-  "7x8": 56,
-  "7x9": 63,
-  "7x10": 70,
-  "8x1": 8,
-  "8x2": 16,
-  "8x3": 24,
-  "8x4": 32,
-  "8x5": 40,
-  "8x6": 48,
-  "8x7": 56,
-  "8x8": 64,
-  "8x9": 72,
-  "8x10": 80,
-  "9x1": 9,
-  "9x2": 18,
-  "9x3": 27,
-  "9x4": 36,
-  "9x5": 45,
-  "9x6": 54,
-  "9x7": 63,
-  "9x8": 72,
-  "9x9": 81,
-  "9x10": 90,
-  "10x1": 10,
-  "10x2": 20,
-  "10x3": 30,
-  "10x4": 40,
-  "10x5": 50,
-  "10x6": 60,
-  "10x7": 70,
-  "10x8": 80,
-  "10x9": 90,
-  "10x10": 100,
-};
+// Save the multiplication table into a const
+const multiplicationTable = {};
+for (let i = 1; i <= 10; i++) {
+  for (let j = 1; j <= 10; j++) {
+    multiplicationTable[`${i}x${j}`] = i * j;
+  }
+}
 
-// Функция  загрузки веселого или грустного котика
+// Function of receiving an image and displaying it
 function showCat(isHappy) {
   const apiKey =
     "live_OYY7AkC7ueU0Kx9oNuJAfrC4yH5LImv60uzF4S5tQ0kHi3cSchoOqUfdLyDhkavU";
@@ -128,58 +32,63 @@ function showCat(isHappy) {
       const catImage = document.createElement("img");
       catImage.src = imageUrl;
       catImage.alt = isHappy ? "Happy cat" : "Sad cat";
-      catImage.style.maxWidth = "400px";
 
       const container = document.getElementById("catImage");
       container.innerHTML = "";
       container.appendChild(catImage);
     })
-    .catch((error) => console.error("Ошибка при получении котэ", error));
+    .catch((error) => console.error("Error while receiving cat", error));
 }
 
-// Объект с картинками за неправильный ответ
-// Запускаем тренировку кнопкой
+// Launching the application
 start.addEventListener("click", () => {
   getRandomQuestion();
+  userAnswer.focus();
 });
-// Выводим случайное равенство и запрашиваем у пользователя решение - окно ввода
+
+// Random Query Function
 function getRandomQuestion() {
   const keys = Object.keys(multiplicationTable);
   currentKey = keys[Math.floor(Math.random() * keys.length)];
   randomExamp.textContent = currentKey;
 }
 
-// Получаем ввод и сравниваем его с правильным ответом
-// Функция для проверки введенного пользователем ответа
+// Function to check user response
 function checkAnswer() {
   const userValue = userAnswer.value;
   const correctAnswer = multiplicationTable[currentKey];
 
+  submitButton.classList.remove("correct", "mistake");
+
   if (Number(userValue) === correctAnswer) {
+    submitButton.classList.add("correct");
     wrongAnswer.textContent = "";
     rightAnswer.textContent = "Correct!!! :)";
     showCat(true);
   } else {
+    submitButton.classList.add("mistake");
     rightAnswer.textContent = "";
     wrongAnswer.textContent = "Mistake :(";
     showCat(false);
   }
+
+  setTimeout(() => {
+    submitButton.classList.remove("correct", "mistake");
+  }, 1500);
+
   userAnswer.value = "";
   getRandomQuestion();
+  userAnswer.focus();
 }
 
-// Обработка нажатия Enter
+// Processing user actions after entering a response
 userAnswer.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
     checkAnswer();
   }
 });
-// Обработка нажатия кнопки Ответ
+
 submitButton.addEventListener("click", function () {
   checkAnswer();
 });
-
-// Если решение правильное - выводим в отдельное окно картинку из массива за правильный ответ,
-//  если неправильное - из массива за неправильный ответ и показываем правильный ответ
-// Повторяем снова
